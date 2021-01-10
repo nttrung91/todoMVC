a40cad07a90e0ce8e3a9d110c3163333374348a8
Trung Nguyen <nttrung91@hotmail.com>
2021-01-09 22:28:59 -0800

    Add Posts

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
-import Link from "next/link";
+import Head from "next/head";
 
-function HomePage() {
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export default function Home({ allPostsData }) {
   return (
-    <div>
-      <h1>
-        <img src="/icons/vercel.svg" alt="Vercel logo" className="logo" />
-        E-Commerce Bulk Upload
-      </h1>
-      <h3>Other pages</h3>
-      Navigate to{" "}
-      <Link href="posts/first-post">
-        <a>first-post</a>
-      </Link>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>My name is Trung</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
 }
 
-export default HomePage;
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import Head from "next/head";
+
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
   return (
-    <div>
-      <h1>First Post</h1>
-      Back to{" "}
-      <Link href="/">
-        <a>home</a>
-      </Link>
-    </div>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div>
+        <h1>First Post</h1>
+        Back to{" "}
+        <Link href="/">
+          <a>home</a>
+        </Link>
+      </div>
+    </Layout>
   );
 };
 
